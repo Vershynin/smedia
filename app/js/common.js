6b33cac59ebc5a2173eb2dafe179c26b722cbfc4
Ruslan Vershynin <ruslan.vershynin@gmail.com>
2018-05-20 18:14:40 +0300

    section-4

--- app/js/common.js
+++ app/js/common.js
@@ -1,116 +1,58 @@
 $(document).ready(function(){
-  $("#menu").on("click","a", function (event) {
-    //отменяем стандартную обработку нажатия по ссылке
-    event.preventDefault();
 
-    //забираем идентификатор бока с атрибута href
-    var id  = $(this).attr('href'),
-
-    //узнаем высоту от начала страницы до блока на который ссылается якорь
-      top = $(id).offset().top;
-
-    $('body,html').animate({scrollTop: top}, 1250);
-  });
-
-  // var swiper = new Swiper('.swiper-container', {
-  //   autoplay: {
-  //     delay: 744300,
-  //     disableOnInteraction: true,
-  //   },
-  //   navigation: {
-  //     nextEl: '.swiper-button-next',
-  //     prevEl: '.swiper-button-prev',
-  //   },
-  // effect: 'fade',
-  // });
-
-
-  // $('#cbackform').validator();
-
-//*AJAX SEND*//
-
-  $("#cbackform").submit(function( e ) { //устанавливаем событие отправки для формы с id=form
-
-
-    // var formNameValue = document.getElementById('firstname').value;
-  	// if (formNameValue.length==0){
-  	// 	var nameErr = document.getElementById("namefild-err");
-    //   var clearValue = document.getElementsByClassName('clearvalue');
-  	// 	nameErr.className += " validation-err";
-  	// 	return false;
-  	// }
-
-    // var formEmailValue = document.getElementById('email').value;
-    // if (formEmailValue.length==0){
-    //   var emailErr = document.getElementById("emailfild-err");
-    //   emailErr.className += " validation-err";
-    //   $(this).find(".clearvalue").css("display","block");
-    //   return false;
-    // }
+  // $('.input-group.').picker({format: "dd.mm.yyyy"});
 
 
 
-    // var clearValue = document.getElementsByClassName('clearvalue');
+  	// var galleryTop = new Swiper('.gallery-top', {
+  	// 	spaceBetween: 0,
+  	// 	loop: 'true',
+  	// 	loopedSlides: $('.gallery-top .swiper-wrapper .swiper-slide').length,
+  	// 	navigation: {
+  	// 		nextEl: '.swiper-button-next',
+  	// 		prevEl: '.swiper-button-prev',
+  	// 	},
+  	// 	autoplay: {
+    //   	delay: 6000,
+    // 	}
+  	// });
 
 
-    //Проверим содержит ли значение введенное в поле email символы @ и .
-    // at=formEmailValue .indexOf("@");
-    // dot=formEmailValue .indexOf(".");
-    //Если поле не содержит эти символы знач email введен не верно
-    // if (at<1 || dot <1){
-    //  alert('valid err email');
-    //   var emailErr = document.getElementById("emailfild-err")
-    //   emailErr.className += " validation-err";
-    //     $(this).find(".clearvalue").css("display","block");
-    //   return false;
-    // }
 
-    // var formNameValue = document.getElementById('tmessage').value;
-    // if (formNameValue.length==0){
-    //   var nameErr = document.getElementById("textareafild-err");
-    //   // var clearValue = document.getElementsByClassName('clearvalue');
-    //   nameErr.className += " validation-err";
-    //   return false;
-    // }
-      var name = $('#firstname').val();
-      var email = $('#email').val();
-      var message = $('#tmessage').val();
+  	// setTimeout(function() {
+  	//
+  	// }, 100);
 
-      e.preventDefault();
+  	var galleryThumbs = new Swiper('.gallery-thumbs', {
+  		spaceBetween: 10,
+  		centeredSlides: true,
+  		loop: 'true',
+      //autoHeight: true,
+  		//loopedSlides: $('.gallery-thumbs .swiper-wrapper .swiper-slide').length,
+  		//slidesPerView: 'auto',
+      slidesPerView: 3,
+      spaceBetween: 30,
+      //width: 'auto',
+  		touchRatio: 0.2,
+  		slideToClickedSlide: true,
+  		autoplay: {
+  			delay: 4000,
+  		},
+    	navigation: {
+    		nextEl: '.swiper-next',
+    		prevEl: '.swiper-prev',
+    	}
+  	});
 
-      if ( name != '' && email != '' && message != '' ) {
-        var form_data = $(this).serialize(); //собираем все данные из формы
-        $.ajax({
-            type: "POST", //Метод отправки
-            url: "mail.php", //путь до php файла отправителя
-            data: form_data,
+  	//loop: 'true',
+  //	var loopedSlidesCount = $('.gallery-thumbs .swiper-wrapper .swiper-slide').length;
+  //	console.log(loopedSlidesCount);
 
-            success: function(res) {
-                //код в этом блоке выполняется при успешной отправке сообщения
-                alert("Ваше сообщение отправлено!");
-                $("#cbackform")[0].reset();
-            },
-            error: function (error) {
-                console.log(error);
-            }
-        });
-      }
-  });
+  	// galleryThumbs.update(true);
+  	// galleryThumbs.slideTo(0, 0);
 
-  // $( "#clr" ).click(function() {
-  //   $("#email").val('');
-  // });
+  //	galleryTop.controller.control = galleryThumbs;
+  //	galleryThumbs.controller.control = galleryTop;
 
-  // $("#firstname").focus(function() {
-  //   $("#namefild-err").removeClass("validation-err");
-  // });
-
-  // $("#email").focus(function() {
-  //   $("#emailfild-err").removeClass("validation-err");
-  // });
-
-  // $("#tmessage").focus(function() {
-  //   $("#textareafild-err").removeClass("validation-err");
-  // });
 
 });
