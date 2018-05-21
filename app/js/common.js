fe266c7141d041263fe259ee7f442db7f2ce9377
Ruslan Vershynin <ruslan.vershynin@gmail.com>
2018-05-21 16:40:34 +0300

    js beautify

--- app/js/common.js
+++ app/js/common.js
@@ -1,38 +1,11 @@
 $(document).ready(function(){
 
-  // $('.input-group.').picker({format: "dd.mm.yyyy"});
-
-
-
-  	// var galleryTop = new Swiper('.gallery-top', {
-  	// 	spaceBetween: 0,
-  	// 	loop: 'true',
-  	// 	loopedSlides: $('.gallery-top .swiper-wrapper .swiper-slide').length,
-  	// 	navigation: {
-  	// 		nextEl: '.swiper-button-next',
-  	// 		prevEl: '.swiper-button-prev',
-  	// 	},
-  	// 	autoplay: {
-    //   	delay: 6000,
-    // 	}
-  	// });
-
-
-
-  	// setTimeout(function() {
-  	//
-  	// }, 100);
-
   	var galleryThumbs = new Swiper('.gallery-thumbs', {
   		spaceBetween: 10,
   		centeredSlides: true,
   		loop: 'true',
-      //autoHeight: true,
-  		//loopedSlides: $('.gallery-thumbs .swiper-wrapper .swiper-slide').length,
-  		//slidesPerView: 'auto',
       slidesPerView: 3,
       spaceBetween: 30,
-      //width: 'auto',
   		touchRatio: 0.2,
   		slideToClickedSlide: true,
   		autoplay: {
@@ -43,24 +16,17 @@ $(document).ready(function(){
     		prevEl: '.swiper-prev',
     	},
       breakpoints: {
-          // when window width is <= 320px
         320: {
           slidesPerView: 1,
-          //spaceBetween: 10
         },
-          // when window width is <= 480px
         480: {
           slidesPerView: 1,
-          //spaceBetween: 20
         },
-          // when window width is <= 640px
           640: {
             slidesPerView: 1,
-          //  spaceBetween: 30
           },
           768: {
             slidesPerView: 3,
-            //spaceBetween: 30
           }
         }
   	});
@@ -70,34 +36,22 @@ $(document).ready(function(){
         delay: 5000,
       },
       loop: 'true',
-      // width: '370px',
       navigation: {
         nextEl: '.career-next',
         prevEl: '.career-prev',
       },
-     // slidesPerView: 'auto'
      slidesPerView: 1
 
   	});
 
-  	//loop: 'true',
-  //	var loopedSlidesCount = $('.gallery-thumbs .swiper-wrapper .swiper-slide').length;
-  //	console.log(loopedSlidesCount);
 
-  	// galleryThumbs.update(true);
-  	// galleryThumbs.slideTo(0, 0);
-
-  //	galleryTop.controller.control = galleryThumbs;
-  //	galleryThumbs.controller.control = galleryTop;
 
   $("#menu").on("click","a", function (event) {
-    //отменяем стандартную обработку нажатия по ссылке
+
     event.preventDefault();
 
-    //забираем идентификатор бока с атрибута href
     var id  = $(this).attr('href'),
 
-    //узнаем высоту от начала страницы до блока на который ссылается якорь - высоту меню
       top = ($(id).offset().top) - 75;
 
     $('body,html').animate({scrollTop: top}, 1250);
