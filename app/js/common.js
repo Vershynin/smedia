71d506c82bf36de1737a3512b0c81d90572bcb93
Ruslan Vershynin <ruslan.vershynin@gmail.com>
2018-05-21 14:26:47 +0300

    responsive fix

--- app/js/common.js
+++ app/js/common.js
@@ -41,7 +41,24 @@ $(document).ready(function(){
     	navigation: {
     		nextEl: '.swiper-next',
     		prevEl: '.swiper-prev',
-    	}
+    	},
+      breakpoints: {
+          // when window width is <= 320px
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 10
+        },
+          // when window width is <= 480px
+        480: {
+          slidesPerView: 2,
+          spaceBetween: 20
+        },
+          // when window width is <= 640px
+          640: {
+            slidesPerView: 3,
+            spaceBetween: 30
+          }
+        }
   	});
 
     var careersThumbs = new Swiper('.career-thumbs', {
